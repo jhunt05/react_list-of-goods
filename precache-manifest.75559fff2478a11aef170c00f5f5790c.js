e7bdd58732df8eb44fdf1ed9b2dcae58c9792f53
Viktoriia <jhunt@ukr.net>
2019-10-31 15:38:43 +0200

    Updates

---
+++ precache-manifest.75559fff2478a11aef170c00f5f5790c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b794b57b363413c7b998f36e4af6fe",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "f7b8d84e895b0486edf3",
+    "url": "/react_list-of-goods/static/css/main.f146880b.chunk.css"
+  },
+  {
+    "revision": "aa19a42a60f1b42b0a5d",
+    "url": "/react_list-of-goods/static/js/2.26a51a8b.chunk.js"
+  },
+  {
+    "revision": "f7b8d84e895b0486edf3",
+    "url": "/react_list-of-goods/static/js/main.9a5bb753.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.75559fff2478a11aef170c00f5f5790c.js"
 );
 
 self.addEventListener('message', (event) => {
